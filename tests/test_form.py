221c4665e14a46f9077e2f7eb6a36d5e51dba8e8
Elizaveta Kazova <e.a.kazova@gmail.com>
2024-04-06 00:10:06 +0500

    Убраны лишние проверки; добавлена конфигурация размера окна браузера; указаны версии библиотек

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,26 +1,26 @@
 import os
 
-from selene import browser, be, have, by
+from selene import browser, have, by
 
 
 def test_success_submit():
     browser.open('/automation-practice-form')
 
-    browser.element('#firstName').should(be.blank).type('Elizaveta').should(have.value('Elizaveta'))
-    browser.element('#lastName').should(be.blank).type('Kazova').should(have.value('Kazova'))
-    browser.element('#userEmail').should(be.blank).type('test@test.com').should(have.value('test@test.com'))
+    browser.element('#firstName').type('Elizaveta')
+    browser.element('#lastName').type('Kazova')
+    browser.element('#userEmail').type('test@test.com')
     browser.element('[for="gender-radio-2"]').click()
-    browser.element('#userNumber').should(be.blank).type('9997776655').should(have.value('9997776655'))
+    browser.element('#userNumber').type('9997776655')
 
     browser.element('#dateOfBirthInput').click()
     browser.element(".react-datepicker__month-select").click().element(by.text('September')).click()
     browser.element(".react-datepicker__year-select").click().element(by.text('1999')).click()
     browser.element('.react-datepicker__day--007').click()
 
-    browser.element('#subjectsInput').should(be.blank).type('Maths').should(have.value('Maths')).press_enter()
+    browser.element('#subjectsInput').type('Maths').press_enter()
     browser.element('[for=hobbies-checkbox-3]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('../files/test.jpg'))
-    browser.element('#currentAddress').should(be.blank).type('Russia').should(have.value('Russia'))
+    browser.element('#currentAddress').type('Russia')
     browser.element('#state').click().element(by.text('Haryana')).click()
     browser.element('#city').click().element(by.text('Karnal')).click()
 
